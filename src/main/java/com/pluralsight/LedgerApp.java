d177339a1cc293b5d4039c4fe92632a6070d6e86
carloscoss <crobertohenriquez@gmail.com>
2026-04-27 10:08:13 -0500

    Setting up the home screen

--- src/main/java/com/pluralsight/LedgerApp.java
+++ src/main/java/com/pluralsight/LedgerApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class LedgerApp {
+    public static void main(String[] args) {
+    // creating variables and scanner.
+
+        Scanner usersInput = new Scanner(System.in);
+        boolean appRunning = true;
+
+        while(appRunning){
+
+            // This is the home screen:
+            System.out.println();
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            String inputHomeScreen = usersInput.nextLine().toUpperCase();
+        }
+
+    }
 }
